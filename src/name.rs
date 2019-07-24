53b7830493c3a9a6d109110cac6bf51b220b0082
Craig Disselkoen <craigdissel@gmail.com>
2019-07-24 11:43:58 -0700

    Derive PartialOrd and Ord for Name

--- src/name.rs
+++ src/name.rs
@@ -1,6 +1,6 @@
 /// Many LLVM objects have a `Name`, which is either a string name, or just a
 /// sequential numbering (e.g. `%3`).
-#[derive(PartialEq, Eq, Clone, Debug, Hash)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug, Hash)]
 pub enum Name {
     /// has a string name
     Name(String),
