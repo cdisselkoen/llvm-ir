d0394ca7bcecad577bd2d0d4f3a1e0e5060ccdc4
Craig Disselkoen <craigdissel@gmail.com>
2019-07-24 21:02:10 -0700

    Update documentation

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["llvm_ir"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"enum\" href=\"llvm_ir/name/enum.Name.html\" title=\"enum llvm_ir::name::Name\">Name</a>&gt; for <a class=\"enum\" href=\"llvm_ir/name/enum.Name.html\" title=\"enum llvm_ir::name::Name\">Name</a>",synthetic:false,types:["llvm_ir::name::Name"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
