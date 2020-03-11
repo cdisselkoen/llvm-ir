0927d0ed826671fc79f8d151cd58248de3aaf56e
Craig Disselkoen <craigdissel@gmail.com>
2020-03-11 13:55:20 -0700

    Update documentation

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["llvm_ir"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"llvm_ir/name/enum.Name.html\" title=\"enum llvm_ir::name::Name\">Name</a>",synthetic:false,types:["llvm_ir::name::Name"]},];
+implementors["llvm_ir"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"llvm_ir/debugloc/struct.DebugLoc.html\" title=\"struct llvm_ir::debugloc::DebugLoc\">DebugLoc</a>",synthetic:false,types:["llvm_ir::debugloc::DebugLoc"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"llvm_ir/name/enum.Name.html\" title=\"enum llvm_ir::name::Name\">Name</a>",synthetic:false,types:["llvm_ir::name::Name"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
