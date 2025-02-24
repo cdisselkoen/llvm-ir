6be053c86da91b9360b827e3938bbd0c341417fb
Qi-Zhan <qizhan@zju.edu.cn>
2025-02-24 20:07:15 +0800

    implement inlineasm in llvm-19

--- tests/llvm_19_tests.rs
+++ tests/llvm_19_tests.rs
@@ -23,10 +23,29 @@ llvm_test!(
 );
 
 #[test]
+fn inline_assembly() {
+    let _ = env_logger::builder().is_test(true).try_init();
+    let path = Path::new("tests/llvm_bc/nop-inlineasm.ll.bc");
+    let module = Module::from_bc_path(path).expect("Failed to parse module");
+    let inst = &module.functions[0].basic_blocks[0].instrs[0];
+    if let llvm_ir::Instruction::Call(call) = inst {
+        assert!(call.function.is_left());
+        let inline = call.function.clone().left().unwrap();
+        assert_eq!(inline.assembly, "nop;");
+        assert_eq!(inline.dialect, llvm_ir::instruction::AssemblyDialect::ATT);
+        assert!(inline.has_side_effects);
+        assert!(!inline.align_stack);
+        assert_eq!(inline.constraints, "");
+    } else {
+        panic!("Expected InlineAsm instruction, got {:?}", inst);
+    }
+}
+
+#[test]
 fn ptr_auth() {
     let _ = env_logger::builder().is_test(true).try_init(); // capture log messages with test harness
     let path = Path::new("tests/llvm_bc/compatibility-as-of-llvm-19.bc");
-    let module = Module::from_bc_path(&path).expect("Failed to parse module");
+    let module = Module::from_bc_path(path).expect("Failed to parse module");
 
     let auth_var = module.global_vars.iter().find(|var| var.name == "auth_var".into())
         .expect("auth_var variable should exist");
